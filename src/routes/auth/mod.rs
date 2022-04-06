741fa3bbe24f080c320b3b683dd9fae574ace70e
Connor Slade <connor@connorcode.com>
2022-04-05 23:14:19 -0400

    🐚 Sesion status

--- src/routes/auth/mod.rs
+++ src/routes/auth/mod.rs
@@ -2,10 +2,12 @@ use crate::{App, Arc, Server};
 
 mod login;
 mod logout;
+mod session;
 mod signup;
 
 pub fn attatch(server: &mut Server, app: Arc<App>) {
     login::attatch(server, app.clone());
     logout::attatch(server, app.clone());
+    session::attatch(server, app.clone());
     signup::attatch(server, app);
 }
---
+++ src/routes/auth/session.rs
@@ -0,0 +1,48 @@
+use afire::prelude::*;
+use serde_json::Value;
+
+use crate::{App, Arc};
+
+pub fn attatch(server: &mut Server, app: Arc<App>) {
+    server.route(Method::POST, "/auth/session", move |req| {
+        // Get session id
+        let body = req.body_string().unwrap();
+        let json: Value = match serde_json::from_str(&body) {
+            Ok(i) => i,
+            Err(e) => {
+                return Response::new()
+                    .status(400)
+                    .text(format!("Error parsing JSON: {}", e))
+            }
+        };
+        let session = match json.get("token") {
+            Some(i) => i.as_str().unwrap(),
+            None => return Response::new().text("No token supplied"),
+        }
+        .to_owned();
+
+        // Get session
+        let session = match app.sessions.lock().iter().find(|x| x.session_id == session) {
+            Some(i) => i,
+            None => return Response::new().status(400).text("Invalid Session"),
+        }
+        .to_owned();
+
+        if session.created.elapsed().as_secs() > app.config.session_timeout {
+            app.sessions
+                .lock()
+                .retain(|x| x.session_id != session.user_id);
+            return Response::new().status(400).text("Session expired");
+        }
+
+        // Send response / Remove cookie
+        Response::new()
+            .text(format!(
+                r#"{{"session": "{}", "userId": "{}", "remainingTime": {}}}"#,
+                session.session_id,
+                session.user_id,
+                app.config.session_timeout - session.created.elapsed().as_secs()
+            ))
+            .content(Content::JSON)
+    });
+}
